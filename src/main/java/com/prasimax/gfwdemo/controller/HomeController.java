e1a1be321340252c25c7e67a18e3eaa55d5d0e0c
Aziz Faozi <admin@labseni.com>
2020-09-29 10:22:51 +0700

    ini pesan commit

--- src/main/java/com/prasimax/gfwdemo/controller/HomeController.java
+++ src/main/java/com/prasimax/gfwdemo/controller/HomeController.java
@@ -6,9 +6,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HomeController {
 
-	@RequestMapping("/")
-	public String index() {
-		return "Odading Mang Soleh";
-	}
+    @RequestMapping("/")
+    public String index() {
+	return "Odading Mang Soleh";
+    }
+
+    @RequestMapping("/v1/api")
+    public String mantan(){
+	return "rasanya strawbery";
+    }
+  
+
+    
 
 }
