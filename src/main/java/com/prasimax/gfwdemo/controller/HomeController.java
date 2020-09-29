b55fd8bbea042577b3fdd3c3fcb0537c02b13977
Aziz Faozi <admin@labseni.com>
2020-09-29 10:14:27 +0700

    mantap

--- src/main/java/com/prasimax/gfwdemo/controller/HomeController.java
+++ src/main/java/com/prasimax/gfwdemo/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.springboot;
+package com.prasimax.gfwdemo.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
