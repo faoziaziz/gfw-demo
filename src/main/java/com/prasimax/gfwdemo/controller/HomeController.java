e082e70a660cda872a54e804d943eb55765f4fb9
Aziz Faozi <admin@labseni.com>
2020-09-29 09:53:33 +0700

    mantan

--- src/main/java/com/prasimax/gfwdemo/controller/HomeController.java
+++ src/main/java/com/prasimax/gfwdemo/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.prasimax.gfwdemo;
+package com.prasimax.gfwdemo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
