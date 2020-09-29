a4753cc1708ccb4b5b1f719dabc6c9731669acdc
Aziz Faozi <admin@labseni.com>
2020-09-29 09:44:41 +0700

    mantap

---
+++ src/test/java/com/prasimax/gfwdemo/GfwDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prasimax.gfwdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GfwDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
