092e7115ae3a99ec220504b4a7ce35fa6f7c8533
Aziz Amerul Faozi <admin@labseni.com>
2020-11-18 04:03:30 +0700

    check for man

---
+++ src/main/java/com/prasimax/gfwdemo/models/Status.java
@@ -0,0 +1,22 @@
+package com.prasimax.gfwdemo.models;
+
+public class Status {
+    private final long kode;
+    private final String status;
+
+    public Status (){
+	this.kode=1;
+	this.status="OK";
+
+    }
+
+    public getKode(){
+	return kode;
+    }
+
+    public getStatus(){
+	return status;
+
+    }
+
+}
